00544dac893b02a4ac80469135f7f90efcd0a0a0
Emil Jansson <emil.jansson@compileit.com>
2023-12-07 15:34:46 +0100

    📝 remove import

--- src/signals.ts
+++ src/signals.ts
@@ -1,4 +1,4 @@
-import { computed, effect, signal } from "@preact/signals-react";
+import { computed, signal } from "@preact/signals-react";
 
 export const signalCount = signal(0);
 export const doubleSignalCount = computed(() => signalCount.value * 2);
