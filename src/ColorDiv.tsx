199a65820004f64ac7f7e575bd25c0239d812ed8
Emil Jansson <emil.jansson@compileit.com>
2023-12-07 09:47:09 +0100

    step 5: react solution 1

--- src/ColorDiv.tsx
+++ src/ColorDiv.tsx
@@ -1,11 +1,15 @@
+const randomColor = () =>
+  `#${Math.random().toString(16).slice(2, 8).padEnd(6, "0")}`;
+
+const color1 = randomColor();
+const color2 = randomColor();
+
 export default function ColorDiv() {
-  const randomColor = () =>
-    `#${Math.random().toString(16).slice(2, 8).padEnd(6, "0")}`;
   return (
     <div
       style={{
-        backgroundColor: randomColor(),
-        color: randomColor(),
+        backgroundColor: color1,
+        color: color2,
       }}
     >
       Did I change color?
